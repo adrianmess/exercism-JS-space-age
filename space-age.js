01216ab3ae4e1e08503846ad334fed288cbe4dae
Adrian Messado <adrian.messado@gmail.com>
2019-04-16 16:49:50 -0700

    space-age

---
+++ space-age.js
@@ -0,0 +1,18 @@
+const age = (planet, seconds) => {
+  let earthYearSeconds = 31557600;
+  const planets = {
+	'earth': 1,
+	'mercury': 0.2408467,
+	'venus': 0.61519726,
+	'mars': 1.8808158,
+	'jupiter': 11.862615,
+	'saturn': 29.447498,
+	'uranus': 84.016846,
+	'neptune': 164.79132
+  };
+
+  let age = seconds / 31557600 / planets[planet]
+  return age;
+};
+
+export { age };
